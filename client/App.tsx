342eaa031424b0b0299293aa7d566b978f6e401c
Ivonne Valenzuela <229033087+IvonneValenzuela@users.noreply.github.com>
2026-08-25 18:06:20 +1200

    Update AboutPaula component import and fix navbar link ID

--- client/App.tsx
+++ client/App.tsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Story from './components/Story'
 import Services from './components/Services'
+import AboutPaula from './components/AboutPaula'
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Story />
       <Services />
+      <AboutPaula />
     </>
   )
 }
